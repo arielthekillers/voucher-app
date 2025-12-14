ab83e248f0e7edbbea4e2f11aef04be2602c80d9
arielthekillers <arielthekillers@gmail.com>
2025-12-15 01:03:44 +0800

    unlink image after deletation

--- public/admin/promos/delete.php
+++ public/admin/promos/delete.php
@@ -8,8 +8,21 @@ auth_required();
 $id = (int) $_GET['id'];
 
 $db = Database::connect();
+
+// Get image filename first
+$stmt = $db->prepare("SELECT image FROM promos WHERE id = ?");
+$stmt->execute([$id]);
+$promo = $stmt->fetch();
+
+if ($promo && $promo['image']) {
+    $filePath = '../../../storage/uploads/promos/' . $promo['image'];
+    if (file_exists($filePath)) {
+        unlink($filePath);
+    }
+}
+
 $stmt = $db->prepare("DELETE FROM promos WHERE id = ?");
 $stmt->execute([$id]);
 
-header('Location: index.php');
+header('Location: index.php?status=deleted');
 exit;
